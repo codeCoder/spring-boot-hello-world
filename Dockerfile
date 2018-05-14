a20b3cbaaccafd623ac0782d1ec5356a721b4ed2
codeCoder <codeCoder@github>
2018-05-15 00:08:47 +0200

    - added cluster role binding - finished port configs - added logback config - disabled diagnostics - updated config to use the rest variant

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,4 @@
-#FROM java:9-jre-apline
-#FROM openjdk:9-jre-alpine
 FROM openjdk:jre-alpine
-#VOLUME /tmp
-#ARG JAR_FILE
-#COPY ${JAR_FILE} app.jar
 COPY target/spring-boot-hello-world.jar app.jar
 EXPOSE 8080
 EXPOSE 5701
--- src/main/java/hello/world/helloworld/cache/HazelcastConfiguration.java
+++ src/main/java/hello/world/helloworld/cache/HazelcastConfiguration.java
@@ -5,6 +5,8 @@ import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.spring.cache.HazelcastCacheManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
@@ -17,6 +19,7 @@ import org.springframework.context.annotation.Profile;
 @Profile("hazelcast")
 public class HazelcastConfiguration {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HazelcastConfiguration.class);
 
     @Value("${HZ_XML_CONFIG:hazelcast-kubernetes.xml}")
     String xmlConfig;
@@ -24,6 +27,43 @@ public class HazelcastConfiguration {
     @Bean
     public Config hazelCastConfig() {
         ClasspathXmlConfig classpathXmlConfig = new ClasspathXmlConfig(xmlConfig);
+/*
+        EntryListenerConfig listenerConfig = new EntryListenerConfig();
+        listenerConfig.setImplementation(new EntryListener() {
+            @Override
+            public void mapEvicted(MapEvent event) {
+                LOGGER.info("mapEvicted on member " + event.getMember().getUuid());
+            }
+
+            @Override
+            public void mapCleared(MapEvent event) {
+                LOGGER.info("mapCleared on member " + event.getMember().getUuid());
+            }
+
+            @Override
+            public void entryUpdated(EntryEvent event) {
+                LOGGER.info("entryUpdated on member " + event.getMember().getUuid() + ", event new value: " + event.getValue() + " old value:" + event.getOldValue());
+            }
+
+            @Override
+            public void entryRemoved(EntryEvent event) {
+                LOGGER.info("entryRemoved on member " + event.getMember().getUuid() + ", event value: " + event.getValue());
+            }
+
+            @Override
+            public void entryEvicted(EntryEvent event) {
+                LOGGER.info("entryEvicted on member " + event.getMember().getUuid() + ", event value: " + event.getValue());
+            }
+
+            @Override
+            public void entryAdded(EntryEvent event) {
+                LOGGER.info("entryAdded on member " + event.getMember().getUuid() + ", event value: " + event.getValue());
+            }
+        });
+
+        listenerConfig.setLocal(false);
+        classpathXmlConfig.addListenerConfig(listenerConfig);
+        */
         return classpathXmlConfig;
     }
 
